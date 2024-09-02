f8c9509349531f0236cd8d5fadf9fd1778f39129
etharabdelazeem <etharabdelazeem1@gmail.com>
2024-09-02 16:08:40 +0000

    [roblem solved

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -26,7 +26,7 @@ def solve_nqueens(board, col, solutions):
                     solution.append([i, j])
         solutions.append(solution)
         return
-    
+
     for i in range(len(board)):
         if is_safe(board, i, col):
             board[i][col] = 1
@@ -47,16 +47,15 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    
+
     try:
         N = int(sys.argv[1])
     except ValueError:
         print("N must be a number")
         sys.exit(1)
-    
+
     if N < 4:
         print("N must be at least 4")
         sys.exit(1)
-    
-    nqueens(N)
 
+    nqueens(N)
