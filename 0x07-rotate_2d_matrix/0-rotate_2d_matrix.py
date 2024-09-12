cbfbd0c0bd37248fe65582f562958489e5ab88c5
etharabdelazeem <etharabdelazeem1@gmail.com>
2024-09-12 07:42:33 +0000

    all done

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+rotating a 2d matrix
+"""
+
+
+def reverseRows(matrix, n):
+    """
+    reverses the rows of a matrix
+    """
+    for i in range(n):
+        j = 0
+        k = n-1
+        while j < k:
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[i][k]
+            matrix[i][k] = temp
+            j += 1
+            k -= 1
+
+
+def transpose(matrix, n):
+    """
+    transposes a matrix
+    """
+    for i in range(n):
+        for j in range(i, n):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[j][i]
+            matrix[j][i] = temp
+
+
+def rotate_2d_matrix(matrix):
+    n = len(matrix)
+    transpose(matrix, n)
+    reverseRows(matrix, n)
